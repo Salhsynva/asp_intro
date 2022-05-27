74a1481df4c2a86701ca7f3b990422b19c68ff83
Salhsynva <6mgc0gt@code.edu.az>
2022-05-27 14:29:35 +0400

    first commit

---
+++ web_intro/Controllers/ExamController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_intro.Models;
+
+namespace web_intro.Controllers
+{
+    public class ExamController : Controller
+    {
+        List<Exam> _exams = new List<Exam>
+        {
+            new Exam(1, "Fizika"),
+            new Exam(2, "Riyaziyyat"),
+            new Exam(3, "Kimya"),
+            new Exam(4, "Tarix"),
+            new Exam(5, "BIalogiya"),
+            new Exam(6, "Cografiya"),
+
+        };
+
+        public ActionResult Index()
+        {
+            ViewBag.Exams = _exams;
+
+            return View();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id != null){
+                Exam wantedExam = _exams.Find(x => x.Id == id);
+                ViewBag.ExamId = wantedExam.Id;
+                ViewBag.ExamSubject = wantedExam.Subject;
+            }
+            return View();  
+        }
+    }
+}
---
+++ web_intro/Models/Exam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_intro.Models
+{
+    public class Exam
+    {
+        public Exam(int id, string subject)
+        {
+            this.Id = id;
+            this.Subject = subject;
+        }
+        public int Id { get; set; }
+        public string Subject { get; set; }
+    }
+}
